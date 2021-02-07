976cad6429faf112ce8076a48239a271b66af8a7
aashutoshPanda <iamashutoshpanda@gmail.com>
2021-02-07 17:35:35 +0530

    minor fix:model to allow long urls

--- meme/models.py
+++ meme/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Meme(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     caption = models.CharField(max_length=100, blank=True, default='')
-    url = models.CharField(max_length=100, blank=True, default='')
+    # because max-length of URL can be 2048
+    url = models.CharField(max_length=2048, blank=True, default='')
     name = models.CharField(max_length=100, blank=True, default='')
 
     class Meta:
