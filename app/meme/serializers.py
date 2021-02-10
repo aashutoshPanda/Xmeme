9d95b7cd5baaa9b4429fe1835267f0dbf9c0c06b
aashutoshPanda <iamashutoshpanda@gmail.com>
2021-02-10 12:50:00 +0530

    changed structure of frontend

--- app/meme/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Meme
-
-
-class MemeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Meme
-        fields = ["name","url","caption","id"]
