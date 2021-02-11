71de5a9f0a9f7b1ebc4fb6f73dfd25f39ffdd044
aashutoshPanda <iamashutoshpanda@gmail.com>
2021-02-11 21:57:05 +0530

    fixed

--- test_server.sh
+++ test_server.sh
@@ -8,25 +8,25 @@
 
 # Run the user’s installation steps which will install any necessary dependencies required for the server to run, with sudo permission
 
-# chmod +x install.sh
+chmod +x install.sh
 
-# sudo ./install.sh
+sudo ./install.sh
 
 
 # 1. Run the user’s server execution steps which will bring up the server
 
 # 2. We’ll be running your server_run.sh as a background process (using &) so that we can run the next set of commands
 
-# chmod +x server_run.sh
+chmod +x server_run.sh
 
-# ./server_run.sh &
+./server_run.sh &
 
 
 # 3. Add a sleep timer to sleep.sh depending upon how long you want to sleep so that the server is ready.
 
-# chmod +x sleep.sh
+chmod +x sleep.sh
 
-# ./sleep.sh
+./sleep.sh
 
 
 # Execute the GET /memes endpoint using curl to ensure your DB is in a clean slate
