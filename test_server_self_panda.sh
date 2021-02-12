69770b52282e8a805bf5a8f12692acfdc0002544
aashutoshPanda <iamashutoshpanda@gmail.com>
2021-02-13 00:46:46 +0530

    removed trailingf slashes

--- test_server_self_panda.sh
+++ test_server_self_panda.sh
@@ -33,12 +33,12 @@ chmod +x sleep.sh
 
 # Should return an empty array.
 
-curl --location --request GET 'http://localhost:8081/memes/'
+curl --location --request GET 'http://localhost:8081/memes'
 
 
 # Execute the POST /memes endpoint using curl
 
-curl --location --request POST 'http://localhost:8081/memes/' \
+curl --location --request POST 'http://localhost:8081/memes' \
 --header 'Content-Type: application/json' \
 --data-raw '{
     "name": "xyz",
@@ -49,7 +49,7 @@ curl --location --request POST 'http://localhost:8081/memes/' \
 
 # Execute the GET /memes endpoint using curl
 
-curl --location --request GET 'http://localhost:8081/memes/'
+curl --location --request GET 'http://localhost:8081/memes'
 
 
 # If you have swagger enabled, make sure it is exposed at localhost:8080
