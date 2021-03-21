4a3c5f5006391d7aa3e48509fdea6db0f4c57bb6
Ashutosh Panda <iamashutoshpanda@gmail.com>
2021-03-21 16:04:53 +0530

    heroku update

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://django-rest-api-crio.herokuapp.com/";
+export const API_BASE_URL = "https://xmeme-server-django.herokuapp.com/";
 
 export const MEMES_PER_PAGE = 100;
